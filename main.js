e39c55a0cc8386a3b7b3301c74c7a546c10d092b
NB-Maestoso <95860271+NB-Maestoso@users.noreply.github.com>
2022-01-17 18:16:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ canvas = new fabric.Canvas('myCanvas');
 ranger_y = 1;
 ranger_x = 1;
 
-block_image_width = 350;
-block_image_height = 430;
+ranger_image_width = 350;
+ranger_image_height = 430;
 
-var block_image_object = "";
+var ranger_image_object = "";
 
 function new_image(get_image) {
     // to upload images
